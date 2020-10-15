9fb6244084e991c50809a5fbcb4cb97dcf113fe2
sinaheidari7 <70712438+sinaheidari7@users.noreply.github.com>
2020-10-14 20:49:03 -0400

    Update Ex3 Java Script

--- ct_Ex3.js
+++ ct_Ex3.js
@@ -1,9 +1,6 @@
-// in Ex2 to Ex5, change the name of the following function properly
+
 function equation() {
-  /* in Ex1, change the following two lines such that 
-     the sum of number num1 and num2 are parsed to an integer 
-     before be assigned to w and h, respectively.        */
-  /* in Ex 2, rename w and h to a and b, respectively.    */
+ 
 
   var a = parseInt(document.getElementById("number1").value);
   var b = parseInt(document.getElementById("number2").value);
@@ -11,11 +8,7 @@ function equation() {
 
   var d = b * b - 4 * a * c;
 
-  /* in Ex2, write a similar code to the above line
-     to capture the value for c*/
-
-  /* in Ex2 to Ex4, you need to replace the following line 
-      with some other formulas */
+ 
   if (d > 0) {
     rootOne = (-1 * b + Math.sqrt(d)) / (2 * a);
     r1 = rootOne.toFixed(2);
@@ -31,23 +24,5 @@ function equation() {
     document.getElementById("output").innerHTML = "root: " + r;
   }
 
-  /* in Ex5, you should delete from Line 9 to this line */
-
-  /*   //in Ex 5, uncomment this block
-  switch (true){
-	case (a>89): 
-		answer="A+";
-		break;
-	case (a>79): 
-		answer="A";
-		break;
-		// in Ex5, you need to add more cases for other letter grades
-	default:
-		answer="F";
-  }
-  */
-
-  /* in Ex2 to Ex5, you need to rename "perimeter" to make it appropriate
-     for those problems, you may also need to rename p to a better 
-	 variable that you might have in your formulas above */
+ 
 }
